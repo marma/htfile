c283831009b59d85ccd924c16cf00ac06ca81136
Martin Malmsten <martin.malmsten@kb.se>
2018-08-11 12:25:11 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='htfile', version='0.0.1', description='HTTP request with Range support as file-like object', author='Martin Malmsten', author_email='martin@martinmalmsten.net', packages=[ 'htfile' ])
